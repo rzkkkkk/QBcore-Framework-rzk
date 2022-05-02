49ddf9cc288be08b0eedc659598deabc9cd6b3eb
Vasco Martins <104381804+rzkkkkk@users.noreply.github.com>
2022-05-02 05:29:08 +0100

    switch qb-menu theme np 3.0

--- resources/[qb]/qb-menu/README.md
+++ resources/[qb]/qb-menu/README.md
@@ -1,84 +1,14 @@
-# qb-menu
-Menu System for the QBCore Framework
+![qb-menu](https://user-images.githubusercontent.com/91661118/139526693-1406c7b1-fb2f-4a3c-be87-c72be59c5e53.png)
 
-This is a modified version of **[NH Context](https://forum.cfx.re/t/no-longer-supported-standalone-nerohiro-s-context-menu-dynamic-event-firing-menu/2564083)** by **[NeroHiro](https://github.com/nerohiro)**
+# lj-fuel used with QBCore Framework
+Join my Discord laboratory for updates, support, and special early testing!
+<br>
+https://discord.gg/loljoshie (without-vanity url: https://discord.gg/HH6uTcBfew)
 
---[[
-EXAMPLE MENU
---]]
+Modified version of qb-menu bringing the most accurate NoPixel 3.0 style possible
 
-```
-RegisterCommand("qbmenutest", function(source, args, raw)
-    openMenu({
-        {
-            header = "Main Title",
-            isMenuHeader = true, -- Set to true to make a nonclickable title
-        },
-        {
-            header = "Sub Menu Button",
-            txt = "This goes to a sub menu",
-            params = {
-                event = "qb-menu:client:testMenu2",
-                args = {
-                    number = 1,
-                }
-            }
-        },
-        {
-            header = "Sub Menu Button",
-            txt = "This goes to a sub menu",
-            disabled = true,
-            -- hidden = true, -- doesnt create this at all if set to true
-            params = {
-                event = "qb-menu:client:testMenu2",
-                args = {
-                    number = 1,
-                }
-            }
-        },
-    })
-end)
-```
-```
-RegisterNetEvent('qb-menu:client:testMenu2', function(data)
-    local number = data.number
-    openMenu({
-        {
-            header = "< Go Back",
-        },
-        {
-            header = "Number: "..number,
-            txt = "Other",
-            params = {
-                event = "qb-menu:client:testButton",
-                args = {
-                    message = "This was called by clicking this button"
-                }
-            }
-        },
-    })
-end)
-```
-```
-RegisterNetEvent('qb-menu:client:testButton', function(data)
-    TriggerEvent('QBCore:Notify', data.message)
-end)
-```
-
-# License
-
-    QBCore Framework
-    Copyright (C) 2021 Joshua Eger
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>
+# Previews
+### qb-menu
+![qb-menu2](https://user-images.githubusercontent.com/91661118/139526514-86fd2936-eede-449e-84de-4a3a1c6f436f.PNG)
+<br>
+![qb-menu1](https://user-images.githubusercontent.com/91661118/139526513-b9a7716d-273b-4f95-acd5-72466cdca299.PNG)
--- resources/[qb]/qb-menu/html/script.js
+++ resources/[qb]/qb-menu/html/script.js
@@ -1,35 +1,58 @@
 let buttonParams = [];
+let menuHistory = [];
 
-const openMenu = (data = null) => {
+const openMenu = (data = null, useHistory = false) => {
     let html = "";
+    if (useHistory) {
+        $("#buttons").html(" ");
+        buttonParams = [];
+        data = menuHistory[menuHistory.length - 2];
+    }
+
     data.forEach((item, index) => {
-        if(!item.hidden) {
-            let header = item.header;
-            let message = item.txt || item.text;
-            let isMenuHeader = item.isMenuHeader;
-            let isDisabled = item.disabled;
-            html += getButtonRender(header, message, index, isMenuHeader, isDisabled);
-            if (item.params) buttonParams[index] = item.params;
-        }
+        let header = item.header;
+        let message = item.txt || item.text;
+        let isMenuHeader = item.isMenuHeader;
+        html += getButtonRender(header, message, index, isMenuHeader);
+        if (item.params) buttonParams[index] = item.params;
     });
 
     $("#buttons").html(html);
-
-    $('.button').click(function() {
-        const target = $(this)
-        if (!target.hasClass('title') && !target.hasClass('disabled')) {
-            postData(target.attr('id'));
-        }
-    });
+    menuHistory.push(data);
 };
 
-const getButtonRender = (header, message = null, id, isMenuHeader, isDisabled) => {
-    return `
-    <div class="${isMenuHeader ? "title" : "button"} ${isDisabled ? "disabled" : ""}" id="${id}">
-        <div class="header">${header}</div>
-        ${message ? `<div class="text">${message}</div>` : ""}
-    </div>
-    `;
+const showHeader = (data = null) => {
+    let html = "";
+    data.forEach((item, index) => {
+        let header = item.header;
+        let message = item.txt || item.text;
+        let isMenuHeader = item.isMenuHeader;
+        html += getButtonRender(header, message, index, isMenuHeader);
+        if (item.params) buttonParams[index] = item.params;
+    });
+    $("#buttons").html(html);
+    menuHistory.push(data);
+}
+
+const getButtonRender = (header, message = null, id, isMenuHeader) => {
+    if (message) {
+        return `
+            <div class="${
+                isMenuHeader ? "title" : "button"
+            }" data-btn-id="${id}">
+                <div class="header">${header}</div>
+                <div class="text">${message}</div>
+            </div>
+        `;
+    } else {
+        return `
+            <div class="${
+                isMenuHeader ? "title" : "button"
+            }" data-btn-id="${id}">
+                <div class="header">${header}</div>
+            </div>
+        `;
+    }
 };
 
 const closeMenu = () => {
@@ -37,8 +60,17 @@ const closeMenu = () => {
     buttonParams = [];
 };
 
+const useHistory = () => {
+    return openMenu(null, true);
+};
+
 const postData = (id) => {
-    $.post(`https://${GetParentResourceName()}/clickedButton`, JSON.stringify(parseInt(id) + 1));
+    if (!buttonParams[id]) return useHistory();
+
+    $.post(
+        `https://${GetParentResourceName()}/clickedButton`,
+        JSON.stringify(buttonParams[id])
+    );
     return closeMenu();
 };
 
@@ -47,7 +79,17 @@ const cancelMenu = () => {
     return closeMenu();
 };
 
+const clearHistory = () => {
+    menuHistory = [];
+};
 
+$(document).click(function (event) {
+    let target = $(event.target);
+    if (target.closest(".button").length && $(".button").is(":visible")) {
+        let btnId = $(event.target).closest(".button").data("btn-id");
+        postData(btnId);
+    }
+});
 
 window.addEventListener("message", (event) => {
     const data = event.data;
@@ -55,10 +97,13 @@ window.addEventListener("message", (event) => {
     const action = data.action;
     switch (action) {
         case "OPEN_MENU":
-        case "SHOW_HEADER":
             return openMenu(buttons);
+        case "SHOW_HEADER":
+            return showHeader(buttons);
         case "CLOSE_MENU":
             return closeMenu();
+        case "CLEAR_HISTORY":
+            return clearHistory();
         default:
             return;
     }
@@ -68,5 +113,6 @@ document.onkeyup = function (event) {
     const charCode = event.key;
     if (charCode == "Escape") {
         cancelMenu();
+        clearHistory();
     }
 };
